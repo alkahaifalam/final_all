e1c55110e8800d645fdac24a9e2975ed7a7d1a29
iacsd <iacsd@iacsd.routereb6334.com>
2024-12-31 09:15:36 +0530

    seventh

--- main.tf
+++ main.tf
@@ -1,22 +1,18 @@
-provider "aws" {
+provider "aws"{
   region = "ap-south-1"
 }
-
 # Webserver Instance
 resource "aws_instance" "Webserver_1" {
   ami           = "ami-03c68e52484d7488f"
   instance_type = "t2.micro"
-
   tags = {
     Name = "Webserver_1"
   }
 }
-
 # Webserver Instance
 resource "aws_instance" "webserver_2" {
   ami           = "ami-03c68e52484d7488f"
   instance_type = "t2.micro"
-
   tags = {
     Name = "webserver_2"
   }
